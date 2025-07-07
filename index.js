04e6d07f4e8a1ff8cd7bc4d6d2f52132ef679957
Pom Ham <pomer@macbookpro.lan>
2025-07-07 11:56:05 -0400

    v3

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const app = express()
 app.get('/', (req, res) => {
 <<<<<<< HEAD
   res.send('<h1>Hello World!v2</h1>')
-=======
-  res.send('<h1>Hello World!v1</h1>')
->>>>>>> 17c91bde966950b5e4252da30e4b4f6a94b8ae58
 })
 
 const PORT = 8080
